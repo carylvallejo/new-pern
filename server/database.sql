5e13a32e0cb0a5daae8042598f66e846fcba6dbc
carylvallejo <carylchrisabelle.vallejo@benilde.edu.ph>
2022-10-20 14:39:12 +0800

    Update database.sql

--- server/database.sql
+++ server/database.sql
@@ -5,3 +5,22 @@ create table users(
     password varchar(255) not null,
     created_at date default current_date
 );
+
+-- create table nts(
+--     id serial primary key,
+--     content varchar(255) not null,
+--     date date default current_date,
+--     important boolean default true
+-- );
+
+-- update nts
+--     set important = (random() < 0.5);
+
+-- update notes_table
+--     set date = datetime;
+
+-- create table persons_tbl(
+--     person_id serial primary key,
+--     name varchar(255) unique not null,
+--     number varchar(255) not null
+-- );
